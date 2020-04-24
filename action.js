f407dee6fc897ec00d6e2d5669a06e16554461fe
Jamie Nelson <jamie@wearsafe.com>
2020-04-24 13:51:24 -0400

    Updated to follow eslint rules

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
-const Jira = require('./common/net/Jira')
-const Octokit = require('@octokit/rest')
 const core = require('@actions/core')
+const Octokit = require('@octokit/rest')
+const Jira = require('./common/net/Jira')
 
 const issueIdRegEx = /([a-zA-Z0-9]+-[0-9]+)/g
 
@@ -11,8 +11,7 @@ const eventTemplates = {
 }
 
 module.exports = class {
-
-  constructor({ githubEvent, argv, config }) {
+  constructor ({ githubEvent, argv, config }) {
     this.Jira = new Jira({
       baseUrl: config.baseUrl,
       token: config.token,
@@ -26,41 +25,41 @@ module.exports = class {
     this.head_ref = argv.head_ref
     this.base_ref = argv.base_ref
     this.gist_private = argv.gist_private
-    this.payload = process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH) : {}
     this.github = null
     this.createGist = false
     this.commitMessageList = null
     this.foundKeys = null
 
     if (argv.github_token && (argv.gist_name || (this.base_ref && this.head_ref))) {
-
       this.github = new Octokit({ auth: `token ${argv.github_token}` })
 
-      if (argv.gist_name)
-        this.createGist = true
+      if (argv.gist_name) this.createGist = true
     }
   }
 
-  get repo() {
+  get repo () {
     if (process.env.GITHUB_REPOSITORY) {
       const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+
       return { owner, repo }
     }
 
-    if (this.payload.repository) {
+    if (this.githubEvent.repository) {
       return {
-        owner: this.payload.repository.owner.login,
-        repo: this.payload.repository.name
+        owner: this.githubEvent.repository.owner.login,
+        repo: this.githubEvent.repository.name,
       }
     }
 
     throw new Error('this.repo requires a GITHUB_REPOSITORY environment variable like \'owner/repo\'')
   }
 
-  async getJiraKeysFromGit() {
+  async getJiraKeysFromGit () {
     let match = null
+
     if (!(this.base_ref && this.head_ref)) {
       core.debug('Base ref and head ref not found')
+
       return
     }
 
@@ -71,59 +70,55 @@ module.exports = class {
       head: this.head_ref,
     })
 
-    if (!commits || !commits.data)
-      return
+    if (!commits || !commits.data) { return }
 
-    let fullArray = new Array()
+    const fullArray = []
+
     match = this.head_ref.match(issueIdRegEx)
 
-    for (const issueKey of match)
-      fullArray.push(issueKey)
+    for (const issueKey of match) { fullArray.push(issueKey) }
 
     for (const commit of commits.data.commits) {
-
-      if (!commit.message)
-        continue
-
-      match = commit.message.match(issueIdRegEx)
-      if (!match)
-        continue
-
-      for (const issueKey of match)
-        fullArray.push(issueKey)
-
+      if (commit.message) {
+        match = commit.message.match(issueIdRegEx)
+        if (match) {
+          for (const issueKey of match) { fullArray.push(issueKey) }
+        }
+      }
     }
     // Make the array Unique
     const uniqueKeys = [...new Set(fullArray)]
+
     core.debug(`Unique Keys: ${uniqueKeys}\n`)
     // Verify that the strings that look like key match real Jira keys
-    this.foundKeys = new Array()
+    this.foundKeys = []
     for (const issueKey of uniqueKeys) {
       const issue = await this.Jira.getIssue(issueKey)
-      if (issue)
-        this.foundKeys.push(issue)
+
+      if (issue) { this.foundKeys.push(issue) }
     }
     core.debug(`Found Jira Keys: ${this.foundKeys}\n`)
+
     return this.foundKeys
   }
 
-  async execute() {
-
+  async execute () {
     const issues = await this.getJiraKeysFromGit()
 
-    if (issues)
-      return issues
+    if (issues) { return issues }
 
     const template = eventTemplates[this.argv.from] || this.argv._.join(' ')
     const extractString = this.preprocessString(template)
+
     if (!extractString) {
-      core.warning(`The event type ${this.githubEvent} is not compatible with this usage`)
+      core.warning(`This github event is not compatible with this usage.`)
+
       return
     }
     const match = extractString.match(issueIdRegEx)
 
     if (!match) {
-      console.log(`String "${extractString}" does not contain issueKeys`)
+      core.warning(`String "${extractString}" does not contain issueKeys`)
 
       return
     }
@@ -137,14 +132,14 @@ module.exports = class {
     }
   }
 
-  preprocessString(str) {
+  preprocessString (str) {
     try {
       _.templateSettings.interpolate = /{{([\s\S]+?)}}/g
       const tmpl = _.template(str)
+
       return tmpl({ event: this.githubEvent })
     } catch (error) {
-      console.error(error)
-      return
+      core.error(error)
     }
   }
 }
