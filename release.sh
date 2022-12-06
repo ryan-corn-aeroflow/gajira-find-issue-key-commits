b6163417dbca75c2595b6b9552727926e3ff258d
Jamie Nelson <jamie@wearsafe.com>
2022-12-06 16:46:15 -0500

    chore(build): fix build steps [fixup]

--- release.sh
+++ release.sh
@@ -3,13 +3,9 @@ git fetch --tags
 
 bump="${1:-patch}"
 yarn version -i "${bump}" || true
-newtag="$(jq -r '.version' package.json)"
-if [[ ${yarntag} != "${newtag#v}" ]]; then
-  yarn version -i "${newtag}" || true
-  yarn build
-fi
+newtag="v$(jq -r '.version' package.json)"
 
-git add dist package.json yarn.lock .yarn
+git add lib package.json yarn.lock .yarn
 git commit -m "chore(release): bump version to ${newtag}" --no-verify
 
 # newtag2="$(git semver get)"
@@ -22,13 +18,13 @@ git commit -m "chore(release): bump version to ${newtag}" --no-verify
 
 stub_major="${newtag%%\.*}"
 stub_major_minor="${newtag%\.*}"
-git tag -d "${stub_major}" 2>/dev/null || true
-git tag -d "${stub_major_minor}" 2>/dev/null || true
+git tag -d "${stub_major}" 2> /dev/null || true
+git tag -d "${stub_major_minor}" 2> /dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
-git push origin ":${stub_major}" 2>/dev/null || true
-git push origin ":${stub_major_minor}" 2>/dev/null || true
-git push origin ":${newtag}" 2>/dev/null || true
+git push origin ":${stub_major}" 2> /dev/null || true
+git push origin ":${stub_major_minor}" 2> /dev/null || true
+git push origin ":${newtag}" 2> /dev/null || true
 git push --tags
 git push
 # yarn release:post
