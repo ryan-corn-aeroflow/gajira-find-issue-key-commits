e97a91f0ee91144018604a0264ccce9559701c3e
Jamie Nelson <jamie@wearsafe.com>
2022-12-07 12:07:10 -0500

    chore(dependency): update dependency

--- release.sh
+++ release.sh
@@ -18,13 +18,13 @@ git commit -m "chore(release): bump version to ${newtag}" --no-verify
 
 stub_major="${newtag%%\.*}"
 stub_major_minor="${newtag%\.*}"
-git tag -d "${stub_major}" 2> /dev/null || true
-git tag -d "${stub_major_minor}" 2> /dev/null || true
+git tag -d "${stub_major}" 2>/dev/null || true
+git tag -d "${stub_major_minor}" 2>/dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
-git push origin ":${stub_major}" 2> /dev/null || true
-git push origin ":${stub_major_minor}" 2> /dev/null || true
-git push origin ":${newtag}" 2> /dev/null || true
+git push origin ":${stub_major}" 2>/dev/null || true
+git push origin ":${stub_major_minor}" 2>/dev/null || true
+git push origin ":${newtag}" 2>/dev/null || true
 git push --tags
 git push
 # yarn release:post
