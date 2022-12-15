168e8d8c44ed99f2e5be7b1ce9e831ef5125fdcb
Jamie Nelson <jamie@wearsafe.com>
2022-12-15 13:41:41 -0500

    chore(release): bumped version to v2.4.1

--- release.sh
+++ release.sh
@@ -22,9 +22,9 @@ git tag -d "${stub_major}" 2> /dev/null || true
 git tag -d "${stub_major_minor}" 2> /dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
-git push origin ":${stub_major}" 2> /dev/null || true
-git push origin ":${stub_major_minor}" 2> /dev/null || true
-git push origin ":${newtag}" 2> /dev/null || true
-git push --tags
-git push
+# git push origin ":${stub_major}" 2> /dev/null || true
+# git push origin ":${stub_major_minor}" 2> /dev/null || true
+# git push origin ":${newtag}" 2> /dev/null || true
+git push -f --tags
+git push -f
 # yarn release:post
